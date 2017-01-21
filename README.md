e0171dd3c6b11c8348e7f92676e7db8a395c7d3a
Jagger De Leo <jagger@twoseven.ca>
2017-01-21 15:00:22 -0500

    Implement basic structure
    More or less an MVP.

---
+++ README.md
@@ -0,0 +1,20 @@
+# OpenWeatherMap Demo App
+
+## Description
+A staightforward Node/Express demo app originally made as an application for [Massless Technologies](http://www.masslesstech.ca). 
+
+## Usage
+- `npm install`
+- Put your OpenWeatherMap API key in `settings.json` 
+- `npm start`
+- Access from http://localhost:3000/
+- Allow HTML5 geolocation access in your browser
+
+## Comments
+This app is more complex than it absolutely needs to be, there's no doubt. However, I took the opportunity to familiarize myself with webpack, build scripts, and  modern Node development in general.
+
+## Credits
+by Jagger De Leo (2017)
+
+## License
+MIT (see [LICENSE](LICENSE))
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var request = require('superagent');
+var debug = require('debug')('mtech-demo:index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Local Weather' });
+});
+
+router.post('/location', function (req, res) {
+  let latitude = req.body.latitude;
+  let longitude = req.body.longitude;
+
+  request.get('api.openweathermap.org/data/2.5/weather')
+    .query({
+      lat: latitude,
+      lon: longitude,
+      APPID: req.app.get('appid')
+    })
+    .end(function(err, weatherResult) {
+      // sets session cookie for lat/lon
+      res.cookie('coords', JSON.stringify({latitude: latitude, longitude: longitude}));
+      if (err) {
+        debug(err);
+        res.send(500, {status: err});
+      } else {
+        // sends full weather data to client for caching 
+        res.cookie('weather', JSON.stringify(weatherResult.body), {maxAge: 1000*60*15}); // 15 mins
+        res.send({status: 'cookie set'});
+      }
+    });
 });
 
 module.exports = router;
