84d094a142590c872fa2d06ec62a0f6e64dc0492
Jagger De Leo <jagger@twoseven.ca>
2017-01-22 19:34:46 -0500

    Remove moment.js locale files from Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         }),
         new webpack.ProvidePlugin({
             Cookies: 'js-cookie'
-        })
+        }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
     ]
 };
